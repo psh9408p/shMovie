de8680d68111b041579416e8a57518ba85e268d9
박성현 <parksh@bagseonghyeon-ui-MacBookPro.local>
2020-09-28 21:46:26 +0900

    Presenter Structure

--- src/Routes/Detail/DetailPresenter.js
+++ src/Routes/Detail/DetailPresenter.js
@@ -1,1 +1,15 @@
-export default () => "Detail";
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const DetailPresenter = ({ result, loading, error }) => null;
+
+DetailPresenter.PropTypes = {
+  nowPlaying: PropTypes.array,
+  pupular: PropTypes.array,
+  upComing: PropTypes.array,
+  loading: PropTypes.bool.isRequired,
+  error: PropTypes.string,
+};
+
+export default DetailPresenter;
--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -1,1 +1,16 @@
-export default () => "Homeaaaaazxzxzxa";
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const HomePresenter = ({ nowPlaying, pupular, upComing, loading, error }) =>
+  null;
+
+HomePresenter.PropTypes = {
+  nowPlaying: PropTypes.array,
+  pupular: PropTypes.array,
+  upComing: PropTypes.array,
+  loading: PropTypes.bool.isRequired,
+  error: PropTypes.string,
+};
+
+export default HomePresenter;
